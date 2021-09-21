ecf9925c03d3e4387dddded1ab98eab538f6a62e
mobynick <mobynick365@gmail.com>
2021-09-21 23:02:04 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
 import numpy as np
-from math import *
 
 
-def get_func_value(func, x):
-    try:
-        y = func(x)
-    except ArithmeticError:
-        return None
-    return y
-    
-    
-def get_integral_by_trap(func, a, b, step):
-    arr_X = np.arange(a, b + step, step)
-    arr_Y = [get_func_value(func, x) for x in arr_X]
+def get_integral_by_trap(func_, a: float, b: float, step: float):
+    x_range: np.ndarray = np.arange(a, b + step, step)
+    y_range: np.ndarray = func_(x_range)
     # тут можно вызвать функцию для записи в таблицу arr_X и arr_Y
-    y_sum = 0
-    # обрабатываю так None и деление на два первого и последнего значения
-    for value in arr_Y:
-        if value != None: y_sum += value
-    if arr_Y[0] != None: y_sum -= arr_Y[0] / 2
-    if arr_Y[-1] != None: y_sum -= arr_Y[-1] / 2
-            
+    y_sum: float = y_range.sum()
+    # обрабатываю так NaN и деление на два первого и последнего значения
+    if np.isnan(y_range)[0]:
+        y_sum -= y_range[0] / 2
+    if np.isnan(y_range)[-1]:
+        y_sum -= y_range[-1] / 2
+
     return step * y_sum
-            
 
 
-def get_derivative_in_point(func, x, step):
-    if get_func_value(func, x) == None:
-        return None
+def get_derivative_in_point(func_, x, step):
     # формула двусторонней разности
-    return (get_func_value(func, x + step) - get_func_value(func, x - step)) / (2 * step)
-    
+    return (func_(x + step) - func_(x - step)) / (2 * step)
 
 
-def culc_integral_or_derivative(func, a, b=None, step=0.1, typ='integral'):
-    if typ == 'integral':
-        return get_integral_by_trap(func, a, b, step)
-    if typ == 'derivative':
+def calc_integral_or_derivative(func_, a, b=None, step=0.1, mode='integral'):
+    if mode == 'integral':
+        return get_integral_by_trap(func_, a, b, step)
+    elif mode == 'derivative':
         # тут можно прописать отдельную функцию для получения arr_X и arr_Y
         # а потом уже одна и та же функция будет записывать значения в таблицу
         # для интегралов и производных
-        return get_derivative_in_point(func, x=a, step=step)
-    pass
+        return get_derivative_in_point(func_, x=a, step=step)
+    else:
+        raise NotImplemented('Данная операция не поддерживается')
 
 
 def show_available_functions(file_name, path=''):
     with open(f'{path}{file_name}', 'r') as r_file:
-        for line in r_file: print(line, end='')
-        
+        for line in r_file:
+            print(line, end='')
 
 
-show_available_functions('functions.txt')
-func = eval('lambda x:' + input('Введите функцию (аргумент x): '))
-i1 = culc_integral_or_derivative(func, -1, 1, 0.001)
-print('Значение интеграла: ', i1)
+if __name__ == '__main__':
+    show_available_functions('functions.txt')
+    func = eval('lambda x:' + input('Введите функцию (аргумент x): '))
+    i1 = calc_integral_or_derivative(func, -1, 1, 0.001)
+    print('Значение интеграла: ', i1)
 
-func = eval('lambda x:' + input('Введите функцию (аргумент x): '))
-i1 = culc_integral_or_derivative(func, 0, typ='derivative')
-print('Значение производной: ', i1)
+    func = eval('lambda x:' + input('Введите функцию (аргумент x): '))
+    i1 = calc_integral_or_derivative(func, 0, mode='derivative')
+    print('Значение производной: ', i1)
