677c0ae05defa23bd762816f373854f7585e028e
Ko-nD <kondakovfox3@mail.ru>
2021-09-22 11:59:34 +0300

    корректная обработка констант; исправил формулу производной

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
-def get_integral_values_on_range(y_range, step: float):
+def get_integral_values_on_range(y_range, step: float, len_x: int):
+    if type(y_range == int):
+        return y_range * step * len_x
     y_sum: float = y_range.sum()
     # обрабатываю так NaN и деление на два первого и последнего значения
     if not np.isnan(y_range)[0]:
@@ -11,19 +13,22 @@ def get_integral_values_on_range(y_range, step: float):
     return step * y_sum
 
 
-def get_derivative_values_on_range(y_range, step):
-    diff_y_range = np.array([(y_range[i - 1] + y_range[i + 1]) for i in range(1, y_range.shape[0] - 1)])
+def get_derivative_values_on_range(y_range, step: float, len_x: int):
+    if type(y_range == int): 
+        return np.array([0] * len_x)
+    diff_y_range = np.array([(y_range[i + 1] - y_range[i - 1]) for i in range(1, y_range.shape[0] - 1)])
     # возвращает список значений производной
-    return diff_y_range / (2 * step) 
+    return diff_y_range / (2 * step)
 
 
 def calc_integral_or_derivative(func_, a, b, step=0.1, mode='integral'):
     x_range: np.ndarray = np.arange(a, b + step, step)
+    len_x = x_range.shape[0]
     y_range: np.ndarray = func_(x_range)
     if mode == 'integral':
-        return get_integral_values_on_range(y_range, step)
+        return get_integral_values_on_range(y_range, step, len_x)
     elif mode == 'derivative':
-        return get_derivative_values_on_range(y_range, step)
+        return get_derivative_values_on_range(y_range, step, len_x)
     else:
         raise NotImplemented('Данная операция не поддерживается')
 
