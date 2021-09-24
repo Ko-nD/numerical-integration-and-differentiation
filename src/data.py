9255e67f9378aa4d4ba2e12328acfb677c50d33b
Ko-nD <kondakovfox3@mail.ru>
2021-09-24 22:41:21 +0300

    пофиксил сохранение значений в таблицу

--- src/data.py
+++ src/data.py
@@ -2,9 +2,9 @@ import csv
 
 
 def csv_data_writer(*args):
-    with open("data/derivative.csv", mode="w") as w_file:
+    with open("data/derivative.csv", mode="a") as w_file:
         file_writer = csv.writer(w_file, lineterminator="\r", delimiter=',')
         for index in range(len(args[0])):
             row = [args[i][index] for i in range(len(args))]
-            file_writer.writerow([index, *row])
+            file_writer.writerow(row)
 
