058271a5e7ae3c92e1faea1d7cd25afc3b1e723f
mobynick <mobynick365@gmail.com>
2021-09-24 13:11:51 +0300

    Обобщение ui логики для переиспользования в следующих заданиях

--- src/main.py
+++ src/main.py
@@ -4,38 +4,46 @@ from src import tests
 from src.ui import *
 
 
-def main():
-    show_legend('../functions.txt')
-    mode = get_value('mode')
-    step = get_value('step')
-    inf = get_value('inf')
+def main(ui: UI):
+    ui.show_legend('../functions.txt')
+    mode = ui.get_value('mode')
+    step = ui.get_value('step')
+    inf = ui.get_value('inf')
     while True:
-        fun = safe_eval(get_value('fun'))
+        fun = ui.safe_eval(ui.get_value('fun'))
         if fun == 0 or fun == -1:
-            show_legend('../functions.txt')
+            ui.show_legend('../functions.txt')
             continue
         elif fun == 1:
             break
         elif fun == 2:
-            mode = get_value('mode')
-            step = get_value('step')
-            inf = get_value('inf')
-        a = get_value('a')
-        b = get_value('b')
+            mode = ui.get_value('mode')
+            step = ui.get_value('step')
+            inf = ui.get_value('inf')
+        a = ui.get_value('a')
+        b = ui.get_value('b')
         try:
             x, y = calc_integral_or_derivative(fun, a, b, step, mode, inf)
             if mode == 'integral':
-                return_calculated_value(y, mode)
+                ui.return_calculated_value(y, mode)
             else:
-                show_graphic(x, y)
+                ui.show_graphic(x, y)
         except NotImplemented as handled_exception:
-            handle_exception(handled_exception)
+            ui.handle_exception(handled_exception)
 
 
 if __name__ == '__main__':
+    ui = UI({
+        'mode': 'Режим расчетов (integral/derivative)',
+        'a': 'Нижняя граница интегрирования/дифференцирования',
+        'b': 'Верхняя граница интегрирования/дифференцирования',
+        'inf': 'Значение, считающееся бесконечностью',
+        'step': 'Шаг интегрирования/дифференцирования',
+        'fun': 'Функция, по которой необходимо произвести расчет'
+    })
     try:
         tests.test_integral(lambda a, b, c, d: calc_integral_or_derivative(a, b, c, d, 'integral'))
     except AssertionError as handled_exception:
-        handle_exception(handled_exception,
-                         'Будьте осторожны при использовании калькулятора - он может выдавать неверные результаты')
-    main()
+        ui.handle_exception(handled_exception,
+                            'Будьте осторожны при использовании калькулятора - он может выдавать неверные результаты')
+    main(ui)
--- src/ui.py
+++ src/ui.py
@@ -5,68 +5,61 @@
 import matplotlib.pyplot as plt
 
 
-VALUES = {
-    'mode': 'Режим расчетов (integral/derivative)',
-    'a': 'Нижняя граница интегрирования/дифференцирования',
-    'b': 'Верхняя граница интегрирования/дифференцирования',
-    'inf': 'Значение, считающееся бесконечностью',
-    'step': 'Шаг интегрирования/дифференцирования',
-    'fun': 'Функция, по которой необходимо произвести расчет'
-}
+class UI:
+    def __init__(self, values_map):
+        self.values = values_map
 
+    def ask_value(self, name):
+        print(f'Введите значение {self.values[name]}')
 
-def ask_value(name):
-    print(f'Введите значение {VALUES[name]}')
+    def get_value(self, name):
+        self.ask_value(name)
+        if name == 'mode' or name == 'fun':
+            return input()
+        else:
+            return float(input())
 
+    @staticmethod
+    def return_calculated_value(y, mode):
+        print(f'Расчитанное значение {"интеграла" if mode == "integral" else "производной"} равно {y}')
 
-def get_value(name):
-    ask_value(name)
-    if name == 'mode' or name == 'fun':
-        return input()
-    else:
-        return float(input())
+    @staticmethod
+    def handle_exception(exception, custom_message=None):
+        print(custom_message if custom_message else exception.message)
 
+    @staticmethod
+    def show_legend(file_name, path=''):
+        with open(f'{path}{file_name}', 'r') as r_file:
+            for line in r_file:
+                print(line, end='')
 
-def return_calculated_value(y, mode):
-    print(f'Расчитанное значение {"интеграла" if mode == "integral" else "производной"} равно {y}')
+    @staticmethod
+    def safe_eval(fun):
+        if fun == '/help':
+            UI.show_legend('../functions.txt')
+            return 0
+        elif fun == '/exit':
+            return 1
+        elif fun == '/edit':
+            return 2
+        try:
+            return eval('lambda x:' + fun)
+        except SyntaxError:
+            print('Вы ошиблись в синтаксисе функции!')
+            return -1
+        except:
+            print('Ошибка ввода')
+            return -1
 
-
-def handle_exception(exception, custom_message=None):
-    print(custom_message if custom_message else exception.message)
-
-
-def show_legend(file_name, path=''):
-    with open(f'{path}{file_name}', 'r') as r_file:
-        for line in r_file:
-            print(line, end='')
-
-
-def safe_eval(fun):
-    if fun == '/help':
-        show_legend('../functions.txt')
-        return 0
-    elif fun == '/exit':
-        return 1
-    elif fun == '/edit':
-        return 2
-    try:
-        return eval('lambda x:' + fun)
-    except SyntaxError:
-        print('Вы ошиблись в синтаксисе функции!')
-        return -1
-    except:
-        print('Ошибка ввода')
-        return -1
-
-
-def show_graphic(x, y):
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.spines['left'].set_position('center')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-    plt.plot(x, y, 'r')
-    plt.show()
+    @staticmethod
+    def show_graphic(x, y):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.spines['left'].set_position('center')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        plt.plot(x, y, 'r')
+        plt.show()
