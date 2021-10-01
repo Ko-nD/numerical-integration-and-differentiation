936dded28334542c7262d37a1e48c86d7f0eb1cc
mobynick <mobynick365@gmail.com>
2021-10-01 12:29:11 +0300

    Добавил ограничение на длину значений производной - больше 1 млн значений будет записано в csv

--- src/aspects/calc_interceptors.py
+++ src/aspects/calc_interceptors.py
@@ -1,15 +1,27 @@
+import numpy as np
+
+
+def wrapped_file_write(file, text):
+    file.write(text)
+    return file
+
+
 def batched_func(operation=lambda a, x: a + x, init=0):
     def decorator(fun):
         def wrapper(data, *args, **kwargs):
             cumulative_value = init
             i = 0
             length = len(data)
+            op = operation
+            if length > 1000000 and type(init) == np.ndarray:
+                cumulative_value = open('derivative.csv', 'a')
+                op = wrapped_file_write
             while length >= 1000:
-                cumulative_value = operation(cumulative_value, fun(data[i:i+1000], *args, **kwargs))
+                cumulative_value = op(cumulative_value, fun(data[i:i+1000], *args, **kwargs))
                 i += 1000
                 length -= 1000
             if length > 0:
-                cumulative_value = operation(cumulative_value, fun(data[i:], *args, **kwargs))
+                cumulative_value = op(cumulative_value, fun(data[i:], *args, **kwargs))
             return cumulative_value
         return wrapper
     return decorator
